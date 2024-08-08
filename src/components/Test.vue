899386b6a6980e34aa180ca093ce27c5a7978081
cquadflieg <cquadflieg@wescale.com>
2024-08-08 10:34:16 +0200

    create example file

---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import type { InjectionKey, Ref } from "vue";
+
+export interface TestProps {
+  test?: string;
+}
+
+export const INJECTION_KEY_TEST: InjectionKey<Ref<string[]>> = Symbol("test");
+</script>
+
+<script setup lang="ts">
+import { provide, ref } from "vue";
+
+const props = withDefaults(defineProps<TestProps>(), {});
+
+const test2: Ref<string[]> = ref([]);
+
+provide(INJECTION_KEY_TEST, test2);
+</script>
